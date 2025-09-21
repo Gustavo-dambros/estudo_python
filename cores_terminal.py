2116ace14faeff926a32bbac4422da3bfb535b0f
dambros_gustavo <232476775+Gustavo-dambros@users.noreply.github.com>
2025-09-21 14:03:11 -0300

    Como mudar cores no terminal
    Como mudar cores terminal

---
+++ cores_terminal.py
@@ -0,0 +1,15 @@
+#como colocar cores no terminal
+print('\n\033[7mAqui está uma lista com as cores\033[m\n')
+print('-+-'*30)
+
+print('\n\033[0;41m Aqui está vermelho \033[m')
+print('\033[0;42m Aqui está verde \033[m')
+print('\033[0;43m Aqui esta amarelo \033[m')
+print('\033[0;44m Aqui esta azul \033[m')
+print('\033[0;45m Aqui esta roxo \033[m')
+print('\033[0;46m Aqui esta ciano \033[m')
+print('\033[0;47m Aqui esta branco \033[m')
+
+print('-=-'*30)
+
+print('\n \033[4;34m Aqui esta azul cm sublinhado \033[m \n')
