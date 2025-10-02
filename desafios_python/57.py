386314da9c5e27c679cae0fecb3ece2c19b47916
dambros_gustavo <232476775+Gustavo-dambros@users.noreply.github.com>
2025-10-02 17:51:39 -0300

    exercicio while

---
+++ desafios_python/57.py
@@ -0,0 +1,8 @@
+#analisa se o sexo é fem or man
+
+sexo=input('qual seu sexo? ').upper()
+
+while sexo not in'MF':
+    sexo=input('Digite o seu sexo')
+
+print('cabou')
---
+++ while.py
@@ -0,0 +1,6 @@
+#aula 56+ sobre while
+
+n=2
+while n<100:
+    print(n)
+    n=n+1
